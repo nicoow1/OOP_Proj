393b120d8e49f50a5ea81a77ff17e93b3c3b51e6
Nico Wertheimer <nbwertheim@gmail.com>
2026-07-10 18:34:37 -0500

    Order.java created

---
+++ src/main/Order.java
@@ -0,0 +1,10 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Order {
+    private final String customerName;
+    private final int tableNumber;
+    private final List<MenuItem> items;
+}
